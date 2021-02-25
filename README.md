b8fce9701c1e7f9d32b6ca856091a3d39459fed7
Marco Lehmann <marco.lehmann@contentful.com>
2021-02-25 15:18:13 +0100

    Add statistics

--- README.md
+++ README.md
@@ -40,21 +40,25 @@ end:    2021-02-25 00:00:00 +0100 CET
 7200 datapoints received
 first:  2021-02-20 00:00:00 +0100 CET
 last:   2021-02-24 23:59:00 +0100 CET
+min: 2.00, max: 89.00, mean: 4.67, median: 3.00
+p99: 22.00, p99.9: 86.00, p99.99: 89.00
 
 Timestamp                     │ Value │ Diff │ CH   │ TC   │ LS   │ Streak
-──────────────────────────────┼───────┼──────┼──────┼──────┼──────┼────────────
+──────────────────────────────┼───────┼──────┼──────┼──────┼──────┼────────
 2021-02-22 16:07:00 +0100 CET │ 86.00 │ 1.00 │ 1    │ 0    │ 1    │ ◼
 2021-02-22 16:08:00 +0100 CET │ 88.00 │ 3.00 │ 2    │ 0    │ 2    │ ◼◼
 2021-02-22 16:09:00 +0100 CET │ 86.00 │ 1.00 │ 3    │ 1    │ 3    │ ◼◼◼
-──────────────────────────────┼───────┼──────┼──────┼──────┼──────┼────────────
+──────────────────────────────┼───────┼──────┼──────┼──────┼──────┼────────
 2021-02-22 16:18:00 +0100 CET │ 87.00 │ 2.00 │ 1    │ 1    │ 3    │ ◼
 2021-02-22 16:19:00 +0100 CET │ 86.00 │ 1.00 │ 2    │ 1    │ 3    │ ◼◼
 2021-02-22 16:20:00 +0100 CET │ 89.00 │ 4.00 │ 3    │ 2    │ 3    │ ◼◼◼
-2021-02-22 16:21:00 +0100 CET │ 86.00 │ 1.00 │ 4    │ 2    │ 4    │ ◼◼◼◼
-──────────────────────────────┴───────┴──────┴──────┴──────┴──────┴────────────
+2021-02-22 16:21:00 +0100 CET │ 86.00 │ 1.00 │ 4    │ 2    │ 4    │ ◼◼◼+
+──────────────────────────────┴───────┴──────┴──────┴──────┴──────┴────────
 CH: consecutive hits, TC: trigger count, LS: longest streak
 
 triggers: 2, streak: 4, events: 7
 ```
 
 The output shows that within the 7200 datapoints 7 have been above the threshold of 85.00. All of them are listed as well. It further shows that the alarm would have been triggered 2 times and the longest streak of consecutive data points equal or above the threshold was 4 within the given time range.
+
+For statistical analysis minimum, maximum, mean, and median for the whole dataset are calculated. As well as the percentiles 99, 99.9, and 99.99.
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -27,6 +29,7 @@ type OutputDatapoint struct {
 	TriggerCount        int
 	LongestStreak       int
 	StreakVisualization string
+	StreakBlocks        string
 }
 
 // OutputLine describes a single output line that can be a datapoint or a separator
@@ -37,7 +40,9 @@ type OutputLine struct {
 func getMaxLength(lines []OutputLine, accessor func(line OutputLine) string) int {
 	maxLength := 0
 	for _, line := range lines {
-		length := len(accessor(line))
+		// unicode characters return more than just 1 to len
+		// this is equivalent to `for r := range s`
+		length := len([]rune(accessor(line)))
 		if length > maxLength {
 			maxLength = length
 		}
@@ -45,14 +50,14 @@ func getMaxLength(lines []OutputLine, accessor func(line OutputLine) string) int
 	return maxLength
 }
 
-func getFormatString(largestValueLength int, largestDiffLength int, separator string) string {
+func getFormatString(largestValueLength int, largestDiffLength int, largestStreakLength int, separator string) string {
 	return "%-30v" + separator +
 		"%-" + fmt.Sprintf("%d", largestValueLength+2) + "v" + separator +
 		"%-" + fmt.Sprintf("%d", largestDiffLength+2) + "v" + separator +
 		"%-6v" + separator +
 		"%-6v" + separator +
 		"%-6v" + separator +
-		"%s"
+		"%-" + fmt.Sprintf("%d", largestStreakLength+2) + "v"
 }
 
 func main() {
@@ -64,10 +69,31 @@ func main() {
 		colorWhite  = "\033[37m"
 	)
 
+	precision := "%.2f"
+
 	if len(os.Args) < 9 {
 		fmt.Println("Missing arguments")
-		fmt.Printf("%s\n\t[namespace]\n\t[metricName]\n\t[dimensionName]\n\t[dimensionValue]\n\t[startTime]\n\t[endTime]\n\t[threshold]\n\t[consecutiveHits]\n\n", os.Args[0])
-		fmt.Printf("Example:\n%s \\\n\tAWS/RDS \\\n\tCPUUtilization \\\n\tDBInstanceIdentifier \\\n\tmy-rds-instance-1 \\\n\t2021-02-20T00:00:00+01:00 \\\n\t2021-02-25T00:00:00+01:00 \\\n\t85 \\\n\t3\n", os.Args[0])
+		fmt.Printf(
+			"%s\n"+
+				"\t[namespace]\n"+
+				"\t[metricName]\n"+
+				"\t[dimensionName]\n"+
+				"\t[dimensionValue]\n"+
+				"\t[startTime]\n"+
+				"\t[endTime]\n"+
+				"\t[threshold]\n"+
+				"\t[consecutiveHits]\n\n", os.Args[0])
+		fmt.Printf(
+			"Example:\n"+
+				"%s \\\n"+
+				"\tAWS/RDS \\\n"+
+				"\tCPUUtilization \\\n"+
+				"\tDBInstanceIdentifier \\\n"+
+				"\tmy-rds-instance-1 \\\n"+
+				"\t2021-02-20T00:00:00+01:00 \\\n"+
+				"\t2021-02-25T00:00:00+01:00 \\\n"+
+				"\t85.00 \\\n"+
+				"\t3\n", os.Args[0])
 		os.Exit(1)
 	}
 
@@ -105,7 +131,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("threshold: %.2f\n", threshold)
+	fmt.Printf("threshold: "+precision+"\n", threshold)
 	fmt.Printf("necessary consecutive hits: %d\n", necessaryConsecutiveHits)
 	fmt.Printf("start:  %v\n", startTime)
 	fmt.Printf("end:    %v\n\n", endTime)
@@ -153,7 +179,9 @@ func main() {
 
 	fmt.Printf("%d datapoints received\n", len(metricsData))
 	fmt.Printf("first:  %v\n", metricsData[len(metricsData)-1].Timestamp.In(localLoc))
-	fmt.Printf("last:   %v\n\n", metricsData[0].Timestamp.In(localLoc))
+	fmt.Printf("last:   %v\n", metricsData[0].Timestamp.In(localLoc))
+
+	// ANALYSIS ---
 
 	var (
 		events          int = 0 // counts number of relevant events
@@ -166,8 +194,27 @@ func main() {
 	// collect output lines
 	outputs := []OutputLine{}
 
+	// statistics
+	var (
+		min    *float64  = nil
+		max    *float64  = nil
+		num    int64     = 0
+		sum    float64   = 0
+		values []float64 = []float64{}
+	)
+
 	for index := range metricsData {
 		reverseIndex := len(metricsData) - index - 1
+
+		if min == nil || *min > metricsData[reverseIndex].Value {
+			min = &metricsData[reverseIndex].Value
+		}
+		if max == nil || *max < metricsData[reverseIndex].Value {
+			max = &metricsData[reverseIndex].Value
+		}
+		num++
+		sum += metricsData[reverseIndex].Value
+		values = append(values, metricsData[reverseIndex].Value)
 
 		if metricsData[reverseIndex].Value >= float64(threshold) {
 			events++
@@ -187,10 +234,14 @@ func main() {
 			}
 
 			blockColor := string(colorYellow)
+			blocks := strings.Repeat("◼", int(consecutiveHits))
 			if consecutiveHits >= necessaryConsecutiveHits {
 				blockColor = string(colorRed)
+				blocks = strings.Repeat("◼", int(necessaryConsecutiveHits))
+				if consecutiveHits > necessaryConsecutiveHits {
+					blocks += "+"
+				}
 			}
-			blocks := strings.Repeat("◼", int(consecutiveHits))
 
 			if consecutiveHits == 1 {
 				outputs = append(outputs, OutputLine{
@@ -207,6 +258,7 @@ func main() {
 					TriggerCount:        triggers,
 					LongestStreak:       streak,
 					StreakVisualization: blockColor + blocks + string(colorReset),
+					StreakBlocks:        blocks,
 				},
 			})
 
@@ -217,21 +269,69 @@ func main() {
 	}
 
 	// OUTPUT ---
-	largestValueLength := getMaxLength(outputs, func(line OutputLine) string {
-		if line.Datapoint == nil {
-			return ""
-		}
-		return fmt.Sprintf("%.2f", line.Datapoint.Value)
-	})
-	largestDiffLength := getMaxLength(outputs, func(line OutputLine) string {
-		if line.Datapoint == nil {
-			return ""
-		}
-		return fmt.Sprintf("%.2f", line.Datapoint.Diff)
-	})
+
+	// sort values in ascending order (for median and percentile)
+	sort.Float64s(values)
+
+	median := float64(0)
+	if num%2 == 0 {
+		median = 0.5 * (values[num/2] + values[num/2+1])
+	} else {
+		median = values[(num+1)/2]
+	}
+
+	p9900 := values[int64(math.RoundToEven(float64(num)*0.99))]
+	p9990 := values[int64(math.RoundToEven(float64(num)*0.999))]
+	p9999 := values[int64(math.RoundToEven(float64(num)*0.9999))]
 
 	fmt.Printf(
-		getFormatString(largestValueLength, largestDiffLength, "│")+"\n",
+		"min: %s, max: %s, mean: %s, median: %s\n",
+		string(colorWhite)+fmt.Sprintf(precision, *min)+string(colorReset),
+		string(colorWhite)+fmt.Sprintf(precision, *max)+string(colorReset),
+		string(colorWhite)+fmt.Sprintf(precision, sum/float64(num))+string(colorReset),
+		string(colorWhite)+fmt.Sprintf(precision, median)+string(colorReset),
+	)
+	fmt.Printf(
+		"p99: %s, p99.9: %s, p99.99: %s\n\n",
+		string(colorWhite)+fmt.Sprintf(precision, p9900)+string(colorReset),
+		string(colorWhite)+fmt.Sprintf(precision, p9990)+string(colorReset),
+		string(colorWhite)+fmt.Sprintf(precision, p9999)+string(colorReset),
+	)
+
+	var (
+		largestValueLength  = 0
+		largestDiffLength   = 0
+		largestStreakLength = 6 // length of “Streak” as column title
+	)
+
+	for _, line := range outputs {
+		if line.Datapoint != nil {
+			valueLength := len([]rune(fmt.Sprintf(precision, line.Datapoint.Value)))
+			if valueLength > largestValueLength {
+				largestValueLength = valueLength
+			}
+
+			diffLength := len([]rune(fmt.Sprintf(precision, line.Datapoint.Diff)))
+			if diffLength > largestDiffLength {
+				largestDiffLength = diffLength
+			}
+
+			streakLength := len([]rune(fmt.Sprintf("%s", line.Datapoint.StreakBlocks)))
+			if streakLength > largestStreakLength {
+				largestStreakLength = streakLength
+			}
+
+			if min == nil || *min > line.Datapoint.Value {
+				min = &line.Datapoint.Value
+			}
+			if max == nil || *max < line.Datapoint.Value {
+				max = &line.Datapoint.Value
+			}
+		}
+	}
+
+	fmt.Printf(
+		getFormatString(largestValueLength, largestDiffLength, largestStreakLength, "│")+"\n",
 		"Timestamp",
 		" Value",
 		" Diff",
@@ -243,21 +343,21 @@ func main() {
 	for _, line := range outputs {
 		if line.Datapoint == nil {
 			fmt.Printf(
-				getFormatString(largestValueLength, largestDiffLength, "┼")+"\n",
+				getFormatString(largestValueLength, largestDiffLength, largestStreakLength, "┼")+"\n",
 				strings.Repeat("─", 30),
 				strings.Repeat("─", largestValueLength+2),
 				strings.Repeat("─", largestDiffLength+2),
 				strings.Repeat("─", 6),
 				strings.Repeat("─", 6),
 				strings.Repeat("─", 6),
-				strings.Repeat("─", 20),
+				strings.Repeat("─", largestStreakLength+2),
 			)
 		} else {
 			fmt.Printf(
-				getFormatString(largestValueLength, largestDiffLength, "│")+"\n",
+				getFormatString(largestValueLength, largestDiffLength, largestStreakLength, "│")+"\n",
 				line.Datapoint.Timestamp,
-				" "+fmt.Sprintf("%.2f", line.Datapoint.Value),
-				" "+fmt.Sprintf("%.2f", line.Datapoint.Diff),
+				" "+fmt.Sprintf(precision, line.Datapoint.Value),
+				" "+fmt.Sprintf(precision, line.Datapoint.Diff),
 				" "+fmt.Sprintf("%d", line.Datapoint.ConsecutiveHits),
 				" "+fmt.Sprintf("%d", line.Datapoint.TriggerCount),
 				" "+fmt.Sprintf("%d", line.Datapoint.LongestStreak),
@@ -266,14 +366,14 @@ func main() {
 		}
 	}
 	fmt.Printf(
-		getFormatString(largestValueLength, largestDiffLength, "┴")+"\n",
+		getFormatString(largestValueLength, largestDiffLength, largestStreakLength, "┴")+"\n",
 		strings.Repeat("─", 30),
 		strings.Repeat("─", largestValueLength+2),
 		strings.Repeat("─", largestDiffLength+2),
 		strings.Repeat("─", 6),
 		strings.Repeat("─", 6),
 		strings.Repeat("─", 6),
-		strings.Repeat("─", 20),
+		strings.Repeat("─", largestStreakLength+2),
 	)
 	fmt.Printf("CH: consecutive hits, TC: trigger count, LS: longest streak\n\n")
 
